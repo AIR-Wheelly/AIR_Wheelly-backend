4d4ffc470a1ae7df79919bf35d7c1442e1ba1d76
dbracic21-foi <dbracic21@student.foi.hr>
2024-11-09 15:03:21 +0100

    Scrum-24 Resolve and fix issue with Configuration

--- AIR_Wheelly_API/Program.cs
+++ AIR_Wheelly_API/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 //Services
 builder.Services.AddScoped<AuthService>();
 //JWT
+var jwtSettings = builder.Configuration.GetSection("Jwt");
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -38,9 +39,9 @@ builder.Services.AddAuthentication(options =>
             ValidateAudience = true,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
-            ValidIssuer = "wheelly",
-            ValidAudience = "user",
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("f0085aed6406bfd10a953bdbd2e8b2c706262beae0dd7ba939e7d557e43f70ad88fd664130779f209491440a95ee75420b44ae566fb320a25bda283eb9268b2eeac32d34095d3417b108a8f7539c5e458648daa24e38061060972c8456127b27deaef1db5b8bdbb6ef4d59dd076daccf66c1a42edd0ed0037ddc3442efec9df822b72ae8f72cb225a81365d2795c7e4a8cb0dd5d306e6fb93b573b8a9264d5a191c9135c333720104447b0481be28a30443fbb51f6a9d6f2f20178b505ea3ce6df5b916730ab3673a9929d56c67ff6c76c358032d7adfcace8f5adab0117643a5d4cbcc8d6574d4f15a51c1d56223bd4ac41ecd66a51aff52cfe63741ebcd88f"))
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings["Key"]))
         };
     });
 var app = builder.Build();
--- AIR_Wheelly_BLL/Services/AuthService.cs
+++ AIR_Wheelly_BLL/Services/AuthService.cs
@@ -4,14 +4,17 @@ using System.Text;
 using AIR_Wheelly_Common.DTO;
 using AIR_Wheelly_Common.Interfaces;
 using AIR_Wheelly_DAL.Models;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace AIR_Wheelly_BLL.Services {
     public class AuthService {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration _configuration;
 
-        public AuthService(IUnitOfWork unitOfWork) {
+        public AuthService(IUnitOfWork unitOfWork, IConfiguration configuration) {
             _unitOfWork = unitOfWork;
+            _configuration = configuration;
         }
 
         public async Task<User> RegisterUser(RegisterUserDTO dto) {
@@ -45,7 +48,7 @@ namespace AIR_Wheelly_BLL.Services {
         public string GenerateJwtToken(int Id)
         {
             var handler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes("f0085aed6406bfd10a953bdbd2e8b2c706262beae0dd7ba939e7d557e43f70ad88fd664130779f209491440a95ee75420b44ae566fb320a25bda283eb9268b2eeac32d34095d3417b108a8f7539c5e458648daa24e38061060972c8456127b27deaef1db5b8bdbb6ef4d59dd076daccf66c1a42edd0ed0037ddc3442efec9df822b72ae8f72cb225a81365d2795c7e4a8cb0dd5d306e6fb93b573b8a9264d5a191c9135c333720104447b0481be28a30443fbb51f6a9d6f2f20178b505ea3ce6df5b916730ab3673a9929d56c67ff6c76c358032d7adfcace8f5adab0117643a5d4cbcc8d6574d4f15a51c1d56223bd4ac41ecd66a51aff52cfe63741ebcd88f");
+            var key = Encoding.ASCII.GetBytes(_configuration["JWT:Key"]);
             var tokenDescriptor = new SecurityTokenDescriptor()
             {
                 Subject = new ClaimsIdentity(new[] { new Claim("id", Id.ToString()) }),
