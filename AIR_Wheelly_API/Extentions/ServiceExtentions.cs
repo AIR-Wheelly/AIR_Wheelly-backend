e8525dfe9ba7c1574833db388d79516a0e0bb5d5
dpiskovic21 <dpiskovic21@foi.hr>
2024-11-16 13:59:39 +0100

    SCRUM-26 add extentions class for registering dependencies to clear up program class

---
+++ AIR_Wheelly_API/Extentions/ServiceExtentions.cs
@@ -0,0 +1,15 @@
+﻿using AIR_Wheelly_BLL.Services;
+using AIR_Wheelly_Common.Interfaces;
+using AIR_Wheelly_DAL.Repositories;
+
+namespace AIR_Wheelly_API.Extentions {
+    public static class ServiceExtentions {
+        public static void AddRepositories(this IServiceCollection services) {
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+
+        public static void AddServices(this IServiceCollection services) {
+            services.AddScoped<AuthService>();
+        }
+    }
+}
