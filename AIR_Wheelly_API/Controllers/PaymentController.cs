581e0d12bf76da189b29b2a761ab44dfcd683436
dpiskovic21 <dpiskovic21@foi.hr>
2025-01-08 17:20:14 +0100

    [SCRUM-54] create payment controller with test endpoint

---
+++ AIR_Wheelly_API/Controllers/PaymentController.cs
@@ -0,0 +1,27 @@
+﻿using AIR_Wheelly_BLL.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIR_Wheelly_API.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly PaymentService _paymentService;
+
+        public PaymentController(PaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClientId()
+        {
+            return Ok(await _paymentService.GenerateClientToken());
+        }
+
+    }
+}
