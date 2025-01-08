24051a315b25be178443fc4abb924520c66d9850
dpiskovic21 <dpiskovic21@foi.hr>
2025-01-08 17:19:46 +0100

    [SCRUM-54]  create service class with generate client token method

---
+++ AIR_Wheelly_BLL/Services/PaymentService.cs
@@ -0,0 +1,35 @@
+﻿using Braintree;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIR_Wheelly_BLL.Services
+{
+    public class PaymentService
+    {
+        private readonly BraintreeGateway _gateway;
+
+        public PaymentService(IConfiguration config)
+        {
+            var braintreeSettings = config.GetSection("Braintree");
+
+            _gateway = new BraintreeGateway()
+            {
+                Environment = Braintree.Environment.SANDBOX,
+                MerchantId = braintreeSettings.GetValue<string>("MerchantID"),
+                PublicKey = braintreeSettings.GetValue<string>("PublicKey"),
+                PrivateKey = braintreeSettings.GetValue<string>("PrivateKey")
+            };
+        }
+
+        public async Task<string> GenerateClientToken()
+        {
+            var clientToken = await _gateway.ClientToken.GenerateAsync();
+
+            return clientToken;
+        }
+    }
+}
