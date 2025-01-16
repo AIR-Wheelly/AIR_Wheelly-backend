4a61f5c416ea617876c7dd4a1ee1d9c63bbd3b21
dbracic21@foi <dbracic21@student.foi.hr>
2025-01-16 18:12:02 +0100

    SCRUM-65 Created initial structure and logic for chat

---
+++ AIR_Wheelly_API/Hubs/ChatHub.cs
@@ -0,0 +1,41 @@
+﻿using AIR_Wheelly_Common.Interfaces.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AIR_Wheelly_API.Hubs;
+[Authorize]
+public class ChatHub : Hub
+{
+    private readonly IChatService _chatService;
+
+    public ChatHub(IChatService chatService)
+    {
+        _chatService = chatService;
+    }
+
+    public async Task SendMessage(Guid reservationId, string message)
+    {
+        var userId = Context.User?.FindFirst("id")?.Value;
+        var isValidUser = userId != null && await _chatService.IsUserPartOfReservationAsync(reservationId, Guid.Parse(userId));
+        if (!isValidUser)
+        {
+            throw new UnauthorizedAccessException("User is not authorized to send messages in this chat");
+        }
+
+        if (userId != null)
+        {
+            var chatMessage = _chatService.CreateMessage(reservationId, Guid.Parse(userId), message);
+            await Clients.Group(reservationId.ToString()).SendAsync("ReceiveMessage", chatMessage);
+        }
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var userId = Context.User?.FindFirst("id")?.Value;
+        if (!string.IsNullOrEmpty(userId) && Guid.TryParse(userId, out var userIdGuid))
+        {
+            await _chatService.AddUserToReservationAsync(Groups,Context.ConnectionId, userIdGuid);
+        }
+        await base.OnConnectedAsync();
+    }
+}
---
+++ AIR_Wheelly_BLL/Services/ChatService.cs
@@ -0,0 +1,49 @@
+﻿using AIR_Wheelly_Common.DTO;
+using AIR_Wheelly_Common.DTO.Response;
+using AIR_Wheelly_Common.Interfaces;
+using AIR_Wheelly_Common.Interfaces.Service;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AIR_Wheelly_BLL.Services;
+
+public class ChatService : IChatService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ChatService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+
+
+    public async Task<bool> IsUserPartOfReservationAsync(Guid reservationId, Guid userId)
+    {
+        var reservation = await _unitOfWork.CarReservationRepository.GetByIdAsync(reservationId);
+        if (reservation == null)
+        {
+            return false;
+        }
+        return reservation.UserId == userId || reservation.CarListingId == userId;
+    }
+
+    public ChatDTO CreateMessage(Guid reservationId, Guid senderId, string message)
+    {
+        return new ChatDTO()
+        {
+            ReservationId = reservationId,
+            SenderId = senderId,
+            Message = message
+        };
+    }
+
+    public async Task AddUserToReservationAsync(IGroupManager groupManager, string connectionId, Guid userId)
+    {
+        var reservations = await _unitOfWork.CarReservationRepository.GetByUserIdAsync(userId);
+        foreach (var reservation in reservations)
+        {
+            await groupManager.AddToGroupAsync(connectionId, reservation.Id.ToString());
+        }
+    }
+}
