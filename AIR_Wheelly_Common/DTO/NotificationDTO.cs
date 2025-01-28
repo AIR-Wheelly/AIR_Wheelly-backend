a1e7d639d9dbfcd053548c4472ee485813df09ab
dbracic21@foi <dbracic21@student.foi.hr>
2025-01-29 00:46:16 +0100

    SCRUM-74 Setting initial DTO for Notifications

---
+++ AIR_Wheelly_Common/DTO/NotificationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AIR_Wheelly_Common.DTO;
+
+public class NotificationDTO
+{
+    public Guid NotificationId { get; set; } = Guid.NewGuid();
+    public Guid OwnerId { get; set; } 
+    public Guid RenterId { get; set; }
+    public string Message { get; set; }
+    public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.UtcNow;
+}
